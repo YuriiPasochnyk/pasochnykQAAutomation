93b71aafe5a5a33e086ea20d0099a9dcc8249489
Yurii Pasochnyk <pasochnyk@gmail.com>
2023-02-10 11:28:30 +0200

    Project task #2

---
+++ conftest.py
@@ -0,0 +1,27 @@
+import pytest
+
+
+class User:
+
+    def __init__(self):
+        self.name = None
+        self.second_name = None
+    
+    def create(self):
+        self.name = 'Yurii'
+        self.second_name = 'Pasochnyk'
+
+    def remove(self):
+        self.name = ''
+        self.second_name = ''
+
+
+@pytest.fixture
+def user():
+    user = User()
+    user.create()
+
+    yield user
+
+    user.remove()
+
